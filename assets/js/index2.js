7db7335b26fa39195df64e3a6fcaec15c277420b
Kumaran KM <kumaranmohan1106@gmail.com>
2021-04-28 02:45:55 +0530

    Update index2.js

--- assets/js/index2.js
+++ assets/js/index2.js
@@ -1,15 +1,6 @@
-// Initialization
-var a = document.getElementById('eemail');
-var b =document.getElementById('locaa');
-
-function fetchInfo(){
-    console.log(a.value);
-    location.replace("search.html")
-}
-   
-
 function datee(){
     var d = new Date();
     var b = d.getFullYear()+"-"+("0" + d.getMonth()).slice(-2)+"-"+("0" +d.getDate()).slice(-2) ;
     document.getElementById('locaa').setAttribute("max",b);
 }
+
