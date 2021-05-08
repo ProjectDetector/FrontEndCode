e953905a4cb82fe1c9faa0229ba2a26c4e596807
Kumaran KM <kumaranmohan1106@gmail.com>
2021-05-08 09:36:11 +0530

    Update index2.js

--- assets/js/index2.js
+++ assets/js/index2.js
@@ -1,6 +1,6 @@
 function datee(){
     var d = new Date();
-    var b = d.getFullYear()+"-"+("0" + d.getMonth()).slice(-2)+"-"+("0" +d.getDate()).slice(-2) ;
+    var b = d.getFullYear()+"-"+("0" + (d.getMonth()+1)).slice(-2)+"-"+("0" +d.getDate()).slice(-2) ;
     document.getElementById('locaa').setAttribute("max",b);
 }
 
